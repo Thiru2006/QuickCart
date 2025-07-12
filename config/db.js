3bc5e0e3d5f33e66e389705e0982792988f7d61d
Thirugnanam V S <122544070+Thiru2006@users.noreply.github.com>
2025-07-12 08:02:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,30 +1,30 @@
 import mongoose from "mongoose";
-import { cache } from "react/cjs/react.production";
-import { global } from "styled-jsx/css";
 
-let cached = global.mongoose
+const globalForMongoose = globalThis;
 
-if (!cached){
-    cached = global.mongoose = {conn:null, promise: null}
+let cached = globalForMongoose.mongoose;
+
+if (!cached) {
+    cached = globalForMongoose.mongoose = { conn: null, promise: null };
 }
 
-async function connectDB(){
-    if (cached.conn){
-        return cached.conn
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
     }
 
-    if (!cached.promise){
+    if (!cached.promise) {
         const opts = {
-            bufferCommands:false
-        }
+            bufferCommands: false,
+        };
 
-        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts)).then(mongoose => {
-            return mongoose
-        })
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then((mongoose) => {
+            return mongoose;
+        });
     }
 
-    cached.conn = await cached.promise
-    return cached.conn
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
 
-export default connectDB
+export default connectDB;
